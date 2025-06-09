6d7cd179122e6ef9bf4d5ab86b95691298b6f1f1
S.Matsumoto <satoshi@satom.eu>
2025-06-09 10:17:46 +0200

    Update app.R

--- app.R
+++ app.R
@@ -6,10 +6,10 @@ library(querychat)
 # rsconnect::writeManifest(appDir = "test5_querychat_updated")
 
 querychat_config <- querychat_init(mtcars, 
-                                   create_chat_func = purrr::partial(
-                                     ellmer::chat_anthropic, 
-                                     model = "claude-3-5-sonnet-20241022"
-                                   ),
+                                   # create_chat_func = purrr::partial(
+                                   #   ellmer::chat_anthropic, 
+                                   #   model = "claude-3-5-sonnet-20241022"
+                                   # ),
                                    greeting = "hello you" )
 
 ui <- page_sidebar(
